c41f0fff1161a837c00bb08b9c4eaad6c4889ee8
Liondance <enzo@ieee.org>
2019-04-09 18:10:14 -0400

    better form validation

--- src/components/login-form.jsx
+++ src/components/login-form.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import Joi from "joi-browser";
 import Input from "./input";
 
 class LoginForm extends Component {
@@ -8,7 +8,51 @@ class LoginForm extends Component {
     errors: {}
   };
 
-  validate = () => {
+  schema = {
+    username: Joi.string()
+      .email()
+      .required()
+      .label("User name"),
+    password: Joi.string()
+      .required()
+      .label("Password")
+  };
+
+  oldValidateChange = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") {
+        return "User name is required";
+      }
+    }
+    if (name === "password") {
+      if (value.trim() === "") {
+        return "Password is required";
+      }
+    }
+    return null;
+  };
+
+  validateChange = ({ name, value }) => {
+    const object = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(object, schema);
+    return error ? error.details[0].message : null;
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const horror = this.validateChange(input);
+    if (horror) {
+      errors[input.name] = horror;
+    } else {
+      delete errors[input.name];
+    }
+    let account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account, errors });
+  };
+
+  oldValidateSubmit = () => {
     const errors = {};
     const { account } = this.state;
     if (account.username.trim() === "") {
@@ -20,15 +64,23 @@ class LoginForm extends Component {
     return errors;
   };
 
-  handleChange = e => {
-    let account = { ...this.state.account };
-    account[e.currentTarget.name] = e.currentTarget.value;
-    this.setState({ account });
+  validateSubmit = () => {
+    const options = { abortEarly: false };
+    const result = Joi.validate(this.state.account, this.schema, options);
+    if (!result.error) return {};
+
+    const errors = {};
+    for (let item of result.error.details) {
+      errors[item.path[0]] = item.message;
+    }
+    console.log(result);
+    console.log(errors);
+    return errors;
   };
 
   handleSubmit = e => {
     e.preventDefault();
-    const errors = this.validate();
+    const errors = this.validateSubmit();
     this.setState({ errors });
     if (Object.keys(errors).length === 0) {
       console.log(`submitted ${this.state.account.username}`);
